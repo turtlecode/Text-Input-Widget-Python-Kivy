3dc987b34e1bb3cf2681c8f03c499a38d2d77dd0
turtlecode <85156399+turtlecode@users.noreply.github.com>
2022-01-10 17:08:36 +0300

    Text Input Widget - Python Kivy
    Text Input Widget - Python Kivy

---
+++ main.py
@@ -0,0 +1,33 @@
+import kivy
+from kivy.app import App
+kivy.require('1.9.0')
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scatter import Scatter
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+
+class TutorialApp(App):
+    def build(self):
+        b = BoxLayout(orientation='vertical')
+        t = TextInput(font_size=50,
+                      size_hint_y=None,
+                      height=100)
+
+        f = FloatLayout()
+        s = Scatter()
+        l = Label(text="Hello !",
+                  font_size=50)
+
+        f.add_widget(s)
+        s.add_widget(l)
+
+        b.add_widget(t)
+        b.add_widget(f)
+
+        t.bind(text=l.setter('text'))
+
+        return b
+
+if __name__ == "__main__":
+    TutorialApp().run()
